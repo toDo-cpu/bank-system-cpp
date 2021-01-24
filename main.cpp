acc0cb80e28e273a0c23b43de588aabe2845aebf
Shimves <dalaipprenti@gmail.com>
2021-01-24 13:42:14 +0100

    home menu added

--- main.cpp
+++ main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
-using namespace std;
+#include <iomanip>
+#include <string>
+class Menu {
+    public :
+        char menuAskByUser ;
+        char setMenu(char menu) {
 
-class Customer {
+            switch(menu) {
+                case '1' : 
+                    setMenuToHome();
+                    prompt(menu);
+                    return menuAskByUser;
+                    break;
+                case ''
+            }
 
-    public:
-        string name;
-        string username;
-        string password;
-        string email;
-
-        double money;
+        }
+    private : 
+        
+        void prompt(char onMenu) {
+        
+            std::cout << "[" << onMenu <<"] : "; std::cin >> menuAskByUser;
+        
+        }
+        void setMenuToHome() {
+        
+            std::cout << "------------------------BANK------------------------" << std::endl;
+            std::cout << "\t 1.See your accounts status"<< std::endl;
+            std::cout << "\t 2.Deposit money on a account"<<  std::endl;
+            std::cout << "\t 3.Make a transfer"<<  std::endl;
+            std::cout << "\t 4.Open an account"<<  std::endl;
+            std::cout << "\t 5.Exit" << std::endl;
+        
+        }
 };
 
 int main () {
 
-    Customer user;
+    char input = '1';
+    Menu draw;
 
-    user.name = "Jack";
-    user.username = "Wilson";
+    do {
 
-    cout << user.name << "|" << user.username;
+        input = draw.setMenu(input);
+
+    } while (input != '5');
+
+    std::cout << "Bye bye !" << std::endl;
 
     return 0;
 }
